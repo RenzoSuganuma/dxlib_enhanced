e7f5304619abcb59f7ce472be6f53e20c1806402
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-10-21 07:00:27 +0900

    Revert "no message"
    This reverts commit c239284b2f37e28066286af5fdef624bc9769037.

---
+++ sgl/sgl_level.cpp
@@ -0,0 +1,58 @@
+﻿#include "DxLib.h"
+#include "sgl_actor.h"
+#include "sgl_level.h"
+
+Level::Level() {}
+Level::~Level() {}
+
+void const Level::MainLoopEntry()
+{
+	auto itr = objects_.begin();
+	while (itr != objects_.end())
+	{
+		(*itr)->Begin();
+		itr++;
+	}
+}
+
+void const Level::MainLoopUpdate(float deltaTime)
+{
+	auto itr = objects_.begin();
+	while (itr != objects_.end())
+	{
+		(*itr)->Tick(deltaTime);
+		(*itr)->Draw();
+		itr++;
+	}
+}
+
+void const Level::MainLoopExit()
+{
+	auto it = objects_.begin();
+	while (it != objects_.end())
+	{
+		(*it)->End();
+		++it;
+	}
+	objects_.clear();
+}
+
+const std::list< Actor* >::iterator
+const Level::AddActor(const Actor * obj)
+{
+	const_cast<Actor*>(obj)->SetPlacedLevel(this);
+	objects_.emplace_back(const_cast<Actor*>(obj));
+	auto it = objects_.end();
+	it--;
+	return it;
+}
+
+void const Level::RemoveActor(const Actor* obj)
+{
+	objects_.remove(const_cast<Actor*>(obj));
+}
+
+void const Level::RemoveActor(const std::list< Actor* >::iterator place)
+{
+	objects_.erase(place);
+}
