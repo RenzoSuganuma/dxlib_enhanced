f66a4773aad438b48f67fea17baa697f69deac41
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-11-12 19:19:51 +0900

    no message

--- ManagedDxlGame/program/game/gm_main.cpp
+++ ManagedDxlGame/program/game/gm_main.cpp
@@ -7,19 +7,14 @@
 #include "gm_main.h"
 // sglライブラリ展開---
 #include "../../sgl/sgl_level.h"
-#include "../../sgl/sgl_actor.h"
-#include "../../sgl/sgl_component.h"
 // ゲーム変数宣言---
 auto level = Level::CreateShared();
-auto actor = Actor::CreateShared();
-auto component = Component::CreateShared();
 
 //------------------------------------------------------------------------------------------------------------
 // ゲーム起動時に１度だけ実行されます
 void gameStart() {
 	srand(time(0));
-	// ゲーム変数セットアップ---
-	level->AddActor(actor.get());
+
 	level->MainLoopInitialize();
 }
 
